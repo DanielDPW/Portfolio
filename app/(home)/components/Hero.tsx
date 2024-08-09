ab0d80792e75f55f0722c48ab76dd12a200bc59d
DanielDPW <150114531+DanielDPW@users.noreply.github.com>
2024-08-09 21:05:17 +0700

    feat: added description of self and resized header

--- app/(home)/components/Hero.tsx
+++ app/(home)/components/Hero.tsx
@@ -4,7 +4,12 @@ export default function Hero() {
   return (
     <div className= "text-center xl:text-left">
         <span>Computer Science Student</span>
-        <h1>Hi! My name is <br/><span>Daniel Pedrosa Wu</span></h1>
+        <h1 className= "h1">Hi! My name is <br/><span>Daniel Pedrosa Wu</span></h1>
+        <p>
+            A passionate and driven Computer Science student with a keen interest in technology. 
+            As someone who is new to the field, I'm excited to dive into the world of Computer Science, 
+            eager to hone my skills and make meaningful contributions to our evergrowing society.
+        </p>
     </div>
   )
 }
