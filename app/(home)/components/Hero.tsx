c42b33edf7ec2016ac3806bb1b6d87f69faba267
DanielDPW <150114531+DanielDPW@users.noreply.github.com>
2024-08-09 22:45:06 +0700

    fix: fixed incorrect scaling on some device

--- app/(home)/components/Hero.tsx
+++ app/(home)/components/Hero.tsx
@@ -6,9 +6,11 @@ export default function Hero() {
     <div className="min-h-[60vh] flex flex-col md:flex-row items-center justify-between p-4">
         <div className="text-center md:text-left flex-1 p-4">
             <span className="block text-sm md:text-base">Computer Science Student</span>
-            <h1 className="text-4xl md:text-6xl font-bold">
+            <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold">
                 Hi! My name is <br />
-                <span className="text-4xl md:text-6xl underline underline-offset-8 decoration-red-800">Daniel Pedrosa Wu</span>
+                <span className="text-3xl md:text-4xl lg:text-6xl underline underline-offset-8 decoration-red-800">
+                    Daniel Pedrosa Wu
+                </span>
             </h1>
             <p className="text-base md:text-lg mt-4">
                 A passionate and driven Computer Science student with a keen interest in technology. 
@@ -16,7 +18,7 @@ export default function Hero() {
                 eager to hone my skills and make meaningful contributions to our ever-growing society.
             </p>
         </div>
-        <div className="w-60 h-60 md:w-80 md:h-80 mt-4 md:mt-0">
+        <div className="w-48 h-48 md:w-60 md:h-60 lg:w-80 lg:h-80 mt-4 md:mt-0 flex justify-center items-center">
             <Image
                 src="/assets/portrait.png"
                 priority
--- app/(home)/components/Navbar.tsx
+++ app/(home)/components/Navbar.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { SiGithub } from "react-icons/si";
 import { SiLinkedin } from "react-icons/si";
 
-
 export default function Navbar() {
   const socials = [
     {
@@ -20,18 +19,18 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className= "py-10  flex justify-between items-center">
-      <h1 className= "text-2xl font-bold underline underline-offset-8 decoration-red-700 -rotate-2 hover:scale-110 translate-x-0">
-        Daniel Pedrosa Wu</h1>
-      <div className= "flex items-center gap-5">
+    <nav className="py-6 md:py-10 flex flex-col md:flex-row justify-between items-center">
+      <h1 className="text-xl md:text-2xl font-bold underline underline-offset-8 decoration-red-700 -rotate-2 hover:scale-110">
+        Daniel Pedrosa Wu
+      </h1>
+      <div className="flex items-center gap-4 md:gap-6 mt-4 md:mt-0">
         {socials.map((social, index) => {
           return (
-              <Link href={social.link} key={index} aria-label={social.label}>
-                <social.Icon className= "w-5 h-5 hover:scale-150 translate-x-0"/>
-              </Link>
-            );
-          }
-        )}
+            <Link href={social.link} key={index} aria-label={social.label}>
+              <social.Icon className="w-4 h-4 md:w-5 md:h-5 hover:scale-150 transition-transform" />
+            </Link>
+          );
+        })}
       </div>
     </nav>
   );
